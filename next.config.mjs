c4d37a0c99350f6426c716309871c15918c0eea8
医生 <aojunjie@linsygroup.cn>
2024-10-23 16:20:27 +0800

    update: 1. 更新打包路径

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 添加输出配置
-  output: 'export',
+  // 根据环境变量决定是否使用静态导出
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   
   // 如果您的项目不是部署在域名根目录,还需要设置 basePath
-  // basePath: '/your-base-path',
+  basePath: '/your-base-path',
 
   // 如果您需要将输出目录更改为 'out',可以添加以下配置
   distDir: 'out',
