b2ab34a8a686fd39d21a17b48e1a9fb097d079d7
医生 <aojunjie@linsygroup.cn>
2024-10-23 11:15:58 +0800

    update: set output dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 添加输出配置
+  output: 'export',
+  
+  // 如果您的项目不是部署在域名根目录,还需要设置 basePath
+  // basePath: '/your-base-path',
+
+  // 如果您需要将输出目录更改为 'out',可以添加以下配置
+  distDir: 'out',
+};
 
 export default nextConfig;
